a4efd18f9868422823b16df15a1a7dd2fe7e84da
kimjiyeun70-svg <kimjiyeun70@gmail.com>
2026-06-18 09:20:02 +0900

    AutoPilot: update data/site-config.js

---
+++ data/site-config.js
@@ -0,0 +1,7 @@
+window.siteConfig = {
+  "siteName": "Claim Rights Portal",
+  "primaryColor": "#1e293b",
+  "bgMain": "#fafafa",
+  "fontHeading": "'Plus Jakarta Sans'",
+  "layoutStyle": "modern"
+};
